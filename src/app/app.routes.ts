3020f78316ffeaf6ca52ee88ee4be0d59091a36d
uziel <utti222033@utvco.edu.mx>
2025-07-22 13:26:16 -0600

    ruta de la pagina princpal modificada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,18 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+   {
+    path: '',
+    redirectTo: '/product',
+    pathMatch: 'full'
+  },
   {
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
+    path: 'product',
+    loadComponent: () => import('./pages/products/components/product-list/product-list.component').then((m) => m.ProductListComponent),
+  }
+  
 ];
