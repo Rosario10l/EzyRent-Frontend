1b386da4168e6118829b23b6f4e3684d9eaec709
Jorge <josshy140@gmail.com>
2025-07-30 19:17:47 -0600

    correcion de auth

--- src/app/guard/auth.guard.spec.ts
+++ src/app/guard/auth.guard.spec.ts
@@ -1,17 +1,42 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 
-import { authGuard } from './auth.guard';
+import { AuthGuard } from './auth.guard';
+import { AuthService } from '../services/auth.service';
 
 describe('authGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => authGuard(...guardParameters));
+  let guard: AuthGuard;
+  let AuthServiceMock: any;
+  let routerMock: any;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    AuthServiceMock = {
+      isLoggedIn: jasmine.createSpy()
+    };
+    routerMock = {
+      navigate: jasmine.createSpy()
+    };
+    TestBed.configureTestingModule({
+      providers: [
+        AuthGuard,
+        { provide: AuthService, useValue: AuthServiceMock },
+        { provide: Router, useValue: routerMock }
+      ],
+    });
+    guard = TestBed.inject(AuthGuard);
   });
 
   it('should be created', () => {
-    expect(executeGuard).toBeTruthy();
+    expect(guard).toBeTruthy();
+  });
+
+  it('debe devolver true si el usuario está conectado', () => {
+    AuthServiceMock.isLoggedIn.and.returnValue(true);
+    expect(guard.canActivate()).toBeTruthy();
+  });
+  it('debe devolver false y navegar a /login si el usuario no ha iniciado sesión', () => {
+    AuthServiceMock.isLoggedIn.and.returnValue(false);
+    expect(guard.canActivate()).toBeFalse();
+    expect(routerMock.navigate).toHaveBeenCalledWith(['/login']);
   });
 });
